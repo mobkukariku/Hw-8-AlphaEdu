c21869e822aeb5679a79aed18a3f159194aaf255
Mobkus <tagankhozhaev@gmail.com>
2024-11-19 23:32:10 +0500

    hw-8

---
+++ index.js
@@ -0,0 +1,49 @@
+// 1
+
+let firstName = 'Damir';
+let lastName = 'Tagankhozhaev';
+let age = 19;
+
+console.log(`Мен зовут ${firstName} ${lastName}. Мне ${age} лет.`);
+
+// 2
+
+let city = 'Atyrau';
+
+console.log("Я родом из города",city);
+
+// 3
+
+let password = 'asdasdasdwd';
+
+let hasAccess = password !== '';
+
+console.log(password, hasAccess);
+
+// 4
+
+let isMember = false;
+
+console.log(isMember.toString());
+
+// 5
+
+let carItems = 0;
+
+console.log(Boolean(carItems));
+
+
+// 6
+
+let averageScore = 89.75695;
+
+console.log(averageScore.toFixed(1));
+
+
+// 7
+
+let greetingMessage = '      Приветствуем вас в мире программирования!        ';
+
+console.log(greetingMessage.trim());
+console.log(greetingMessage.toUpperCase());
+console.log(greetingMessage.startsWith('программирования'));
